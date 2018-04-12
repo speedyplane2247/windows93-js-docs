8b9d9dd156cbc9517e5ba3ef1ec6e11dbdfd55f8
DarkPhoenix10 <thedarkpheonix123@gmail.com>
2018-04-12 18:22:31 +0100

    is this how

--- conf.py
+++ conf.py
@@ -4,4 +4,7 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
-source_suffix = ['.rst', '.md']
+source_suffix = '.md'
+master_doc = 'index'
+project = 'Windows 93 JavaScript Documentation'
+copyright = 'DarkPhoenix10'
